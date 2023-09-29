78a9e8f4cd188524f86fb768d59e04197100cd45
Subin <70842920+bin-pro@users.noreply.github.com>
2023-09-29 21:10:24 +0900

    Init: moongkl-here-mobileapi project initializing
    java11, jdk11

---
+++ src/main/java/com/blackshoe/moongklheremobileapi/MoongklHereMobileapiApplication.java
@@ -0,0 +1,13 @@
+package com.blackshoe.moongklheremobileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoongklHereMobileapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoongklHereMobileapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blackshoe/moongklheremobileapi/MoongklHereMobileapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackshoe.moongklheremobileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoongklHereMobileapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

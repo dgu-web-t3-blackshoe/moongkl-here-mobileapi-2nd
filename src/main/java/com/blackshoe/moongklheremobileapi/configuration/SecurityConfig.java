76fb4c71a94dc678f41514e466e0df9b5d44a0fa
Subin <70842920+bin-pro@users.noreply.github.com>
2023-10-04 15:45:52 +0900

    Chore: Add security configuration for json web token and spring security

--- src/main/java/com/blackshoe/moongklheremobileapi/configuration/SecurityConfig.java
+++ src/main/java/com/blackshoe/moongklheremobileapi/configuration/SecurityConfig.java
@@ -1,19 +1,35 @@
 package com.blackshoe.moongklheremobileapi.configuration;
 
+import com.blackshoe.moongklheremobileapi.security.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
+@RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtTokenFilter jwtTokenFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //TODO: CORS 설정 및 Permit 설정 필요
         http
+                .formLogin().disable()
                 .csrf().disable()
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
                 .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin().disable();
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterAfter(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }

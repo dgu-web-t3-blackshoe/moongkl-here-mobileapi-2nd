654c187148b78604a4a92487e763b19e4f49971f
Hankyul Min <denirokr22@gmail.com>
2023-10-15 22:43:22 +0900

    Chore: PostRepository.findAllUserPostByLocation() test code

--- src/test/java/com/blackshoe/moongklheremobileapi/repository/PostRepositoryTest.java
+++ src/test/java/com/blackshoe/moongklheremobileapi/repository/PostRepositoryTest.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.joda.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +20,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 public class PostRepositoryTest {
 
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
@@ -508,14 +518,108 @@ public class PostRepositoryTest {
         final Integer page = 0;
         final Pageable pageable = PageRequest.of(page, size, sortBy);
         final PostPointFilter postPointFilter = PostPointFilter.builder()
-                .latitude(0.8)
-                .longitude(0.8)
+                .latitude(0.98)
+                .longitude(0.98)
                 .radius(3.0)
                 .build();
 
         // when
         final Page<PostDto.PostListReadResponse> postListReadResponsePage
                 = postRepository.findAllBySkinTimeBetweenAndCurrentLocationAndIsPublic(postTimeFilter, postPointFilter, pageable);
+        final String result = objectMapper.writeValueAsString(postListReadResponsePage);
+
+        // then
+        assertThat(postListReadResponsePage.getContent()).isNotNull();
+        log.info("postListReadResponsePage = {}", result);
+    }
+
+    @Test
+    public void findAllUserPostByLocation_whenSuccess_isNotNull() throws JsonProcessingException {
+        // given
+        final SkinLocation domestic = SkinLocation.builder()
+                .latitude(1.0)
+                .longitude(1.0)
+                .country("대한민국")
+                .state("서울특별시")
+                .city("강남구")
+                .build();
+
+        final SkinLocation foreign = SkinLocation.builder()
+                .latitude(1000.0)
+                .longitude(1000.0)
+                .country("미국")
+                .state("캘리포니아")
+                .city("로스앤젤레스")
+                .build();
+
+        final SkinTime year2020 = SkinTime.builder()
+                .year(2020)
+                .month(1)
+                .day(1)
+                .hour(1)
+                .minute(1)
+                .build();
+
+        final SkinTime year2023 = SkinTime.builder()
+                .year(2023)
+                .month(1)
+                .day(1)
+                .hour(1)
+                .minute(1)
+                .build();
+
+        final User user = User.builder()
+                .email("test")
+                .password("test")
+                .nickname("test")
+                .phoneNumber("test")
+                .build();
+
+        final User savedUser = userRepository.save(user);
+
+        final UUID userId = savedUser.getId();
+
+        final SkinUrl skinUrl = SkinUrl.builder()
+                .s3Url("test")
+                .cloudfrontUrl("test")
+                .build();
+
+        final StoryUrl storyUrl = StoryUrl.builder()
+                .s3Url("test")
+                .cloudfrontUrl("test")
+                .build();
+
+        for (int idx = 0; idx < 50; idx++) {
+            Post post = Post.builder()
+                    .skinUrl(skinUrl)
+                    .storyUrl(storyUrl)
+                    .user(savedUser)
+                    .skinLocation(idx % 2 == 0 ? domestic : foreign)
+                    .skinTime(idx % 2 == 0 ? year2020 : year2023)
+                    .likeCount((long) (Math.random() * 100))
+                    .viewCount((long) (Math.random() * 100))
+                    .isPublic(true)
+                    .createdAt(LocalDateTime.now())
+                    .build();
+            postRepository.save(post);
+        }
+
+        final Sort sortBy = Sort.by(Sort.Direction.DESC, "createdAt");
+        final Integer size = 10;
+        final Integer page = 0;
+        final Pageable pageable = PageRequest.of(page, size, sortBy);
+        final PostPointFilter postPointFilter = PostPointFilter.builder()
+                .latitude(0.98)
+                .longitude(0.98)
+                .radius(3.0)
+                .build();
+
+        // when
+        log.info("userId = {}", userId);
+        final List<Post> posts = postRepository.findAllByUser(savedUser);
+        log.info("post userId = {}", posts.get(0).getUser().getId());
+        final Page<PostDto.PostListReadResponse> postListReadResponsePage
+                = postRepository.findAllUserPostByLocation(savedUser, postPointFilter, pageable);
         final String result = objectMapper.writeValueAsString(postListReadResponsePage);
 
         // then

77f3b96a5b575ce235aedab90dbd3f447e47aa11
Subin <70842920+bin-pro@users.noreply.github.com>
2024-01-06 23:17:21 +0900

    :bug: Chore: fix bugs for adding environment variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
 else
   echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
